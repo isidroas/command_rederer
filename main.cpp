e7a62967ba1d2ae15ca3567fd2bd4e2a05465721
Isidro Arias <isidroariass@hotmail.es>
2021-06-21 21:34:59 +0200

    simple test

--- main.cpp
+++ main.cpp
@@ -215,38 +215,21 @@ int main() {
   cout << "************************\n\n";
 
   LedMatrix led_matrix;
-  led_matrix.print();
   struct color some_color {
-    0, 50, 200
-  };
-  led_matrix.fill(some_color);
-  led_matrix.print();
-
-  struct color_hsv some_hsv {
-    65.0, 0.4, 0.35
-  };
-  led_matrix.fill(some_hsv);
-  led_matrix.print();
-  struct color_hsv dhsv {
-    5.0, 0.1, 0.1
-  };
-  led_matrix.increase_hsv(dhsv);
-  led_matrix.print();
-  led_matrix.send_matrix();
-  sleep(1);
-
-  dhsv =  {
-    0, 0, 0.2
-  };
-  led_matrix.increase_hsv(dhsv);
-  led_matrix.send_matrix();
-
-  sleep(5);
-  some_color =  {
     255, 0, 0
   };
   led_matrix.fill(some_color);
   led_matrix.send_matrix();
+  led_matrix.print();
+  
+
+  sleep(5);
+  some_color =  {
+    0, 255, 0 
+  };
+  led_matrix.fill(some_color);
+  led_matrix.send_matrix();
+  led_matrix.print();
 
   sleep(5);
   some_color =  {
@@ -254,13 +237,22 @@ int main() {
   };
   led_matrix.fill(some_color);
   led_matrix.send_matrix();
+  led_matrix.print();
 
-  sleep(5);
-  some_color =  {
-    0, 255, 0
-  };
-  led_matrix.fill(some_color);
-  led_matrix.send_matrix();
+//  struct color_hsv some_hsv {
+//    65.0, 0.4, 0.35
+//  };
+//  led_matrix.fill(some_hsv);
+//  led_matrix.print();
+//  struct color_hsv dhsv {
+//    5.0, 0.1, 0.1
+//  };
+//  led_matrix.increase_hsv(dhsv);
+//  led_matrix.print();
+//  led_matrix.send_matrix();
+//  sleep(1);
+
+
 
   return 0;
 }
